fa723b7a5d2bf9a831bca5a07066566237d84e39
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-24 00:56:46 +0530

    Time: 0 ms (100%), Space: 24.8 MB (87.16%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -1,51 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
 
-    unordered_map<int, int> mp;
-
-    int height(TreeNode* root){
-        if(!root) return 0;
-
-        int l = height(root -> left);
-        int r = height(root -> right);
-
-        return max(l, r) + 1;
-    }
-
-    void solve(TreeNode* root, int level){
+    void solve(TreeNode* root, int depth, int& maxDepth, int &result){
 
         if(!root) return ;
 
-        if(root -> left == NULL && root -> right == NULL){
-            if(mp.find(level) == mp.end()){
-                mp[level] = root -> val;
-                return;
-            }
+        if(depth > maxDepth){
+            maxDepth =  depth;
+            result = root -> val;
         }
-        solve(root -> left, level+1);
-        solve(root -> right, level+1);
+        solve(root -> left, depth+1, maxDepth, result);
+        solve(root -> right, depth+1, maxDepth, result);
     }
 
     int findBottomLeftValue(TreeNode* root) {
         
         if(root == NULL)return 0;
 
-        int depth = height(root);
-        solve(root, 1);
+        int result = INT_MIN;
+        int maxDepth =  -1;
+        solve(root, 1, maxDepth, result);
 
-        int ans = mp[depth];
-
-        return ans;
+        return result;
      }
 };
