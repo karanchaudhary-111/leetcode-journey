a9365ca74c67f323dfdec50d7723ff7dddb6392f
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-25 00:07:16 +0530

    Time: 390 ms (85.39%), Space: 48.5 MB (62.92%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        
+
         int n = nums.size();
+        int mx = 0;
+        for (auto x : nums)
+            mx = max(mx, x);
 
-        unordered_set<int> temp;
-        unordered_set<int> res;
+        // now find the power of 2 just greater than mx;
+        int u = 1;
+        while (u <= mx)
+            u <<= 1;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                temp.insert(nums[i] ^ nums[j]);
+        vector<int> s(u);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                s[nums[i] ^ nums[j]] = 1;
             }
         }
 
-        for(int pair : temp){
-            for(int& x: nums){
-                res.insert(pair ^ x);
-            }
+        vector<int> t(u);
+
+        for (int i = 0; i < u; i++) {
+            if (s[i] == 0)
+                continue;
+
+            for (auto x : nums)
+                t[x ^ i] = 1;
         }
 
-        return res.size();
+        int ans = 0;
+        for (int i = 0; i < u; i++) {
+            if (t[i])
+                ans++;
+        }
+
+        return ans;
     }
 };
