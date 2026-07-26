55c68e84eb6975e6ef6e6a32671c10b9c7d674c1
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-26 15:25:44 +0530

    Time: 0 ms (100%), Space: 32.3 MB (45.57%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool solve(ListNode* head, TreeNode* root){
+        if(!head) return true;
+        if(!root) return false;
+
+        if(root -> val != head -> val){
+            return false;
+        }
+
+        return solve(head -> next, root -> left) || solve(head -> next, root -> right);
+
+    }
+
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        
+        if(!root) return false;
+
+        return solve(head, root) || isSubPath(head, root -> left) || isSubPath(head, root -> right);
+    }
+};
