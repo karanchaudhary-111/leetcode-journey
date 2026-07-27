3c164d985edd40dc4a8ca1db1a39beaf7a4fd33d
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-28 00:43:48 +0530

    Time: 19 ms (72.23%), Space: 208.2 MB (69.07%) - LeetHub

---
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    int storeLVL[1000001];
+    int height[1000001];
+    int levelMaxHt[1000001];
+    int levelSecMaxHt[1000001];
+
+    int findHeight(TreeNode* root, int level){
+        if(!root) return 0;
+
+        storeLVL[root -> val] = level;
+        height[root -> val] = max(findHeight(root -> left, level+1), findHeight(root -> right, level+1)) + 1;
+
+        if(levelMaxHt[level] <  height[root -> val]){
+
+            levelSecMaxHt[level] = levelMaxHt[level];
+            levelMaxHt[level] =  height[root -> val];
+
+        }else if(levelSecMaxHt[level] <  height[root -> val]){
+
+            levelSecMaxHt[level] =  height[root -> val];
+
+        }
+
+        return height[root-> val];
+    }
+
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        
+        vector<int> ans;
+        findHeight(root, 0);
+
+        for(int &node: queries){
+            int L = storeLVL[node];
+            //int H = height[node] == levelMaxHt[L] ? levelSecMaxHt[L] : levelMaxHt[L];
+
+            int temp = L + (height[node] == levelMaxHt[L] ? levelSecMaxHt[L] : levelMaxHt[L]) - 1;
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
