5913bde5c8e58dd7ca2992803777e3b4a59c3cb6
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-27 21:13:50 +0530

    Time: 0 ms (100%), Space: 15.2 MB (95.17%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -18,14 +18,10 @@ public:
 
         if(root1 == NULL || root2 == NULL) return false;
 
-        if(root1 -> val == root2 -> val){
-            bool withoutFlip = flipEquiv(root1 -> left, root2 -> left) && flipEquiv(root1 -> right, root2 -> right);
-            
-            bool withFlip = flipEquiv(root1 -> left, root2 -> right) && flipEquiv(root1 -> right, root2 -> left);
-
-            return withoutFlip || withFlip;
-        }
-        
-        return false;
+         return (root1->val==root2->val) && 
+       (
+        (flipEquiv(root1->left,root2->left ) && flipEquiv(root1->right,root2->right))||
+       (flipEquiv(root1->left,root2->right) && flipEquiv(root1->right , root2->left)) 
+       );
     }
 };
