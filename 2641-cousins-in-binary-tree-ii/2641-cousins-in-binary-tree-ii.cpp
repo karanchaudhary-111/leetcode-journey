7d69223ce891c898288c96bf07aaa0e634ad531c
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-27 20:31:48 +0530

    Time: 8 ms (91.72%), Space: 304.4 MB (96.54%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -18,55 +18,34 @@ public:
         
         queue<TreeNode*> q;
         q.push(root);
-
-        vector<int> levelSum;
+        int levelSum = root -> val;
 
         while(!q.empty()){
             int n = q.size();
-            int currSum = 0;
+            int nextSum = 0;
 
             while(n--){
                 TreeNode* curr = q.front();
                 q.pop();
 
-                currSum += curr -> val;
+                curr -> val = levelSum - curr -> val;
 
-                if(curr -> left != NULL){
-                    q.push(curr -> left);
-                }
-
-                if(curr -> right != NULL){
-                    q.push(curr -> right);
-                }
-            }
-            levelSum.push_back(currSum);
-        }
-
-        q.push(root);
-        root -> val = 0;
-        int i = 1;
-
-        while(!q.empty()){
-            int n = q.size();
-
-            while(n--){
-                TreeNode* curr = q.front();
-                q.pop();
-
-                int siblingSum = curr -> left != NULL ? curr -> left -> val : 0;
+                int siblingSum = curr -> left != NULL ? curr -> left -> val: 0;
                 siblingSum += curr -> right != NULL ? curr -> right -> val : 0;
 
-                if(curr ->left){
-                    curr -> left -> val = levelSum[i] - siblingSum;
-                    q.push(curr -> left);
-                }
+                nextSum += siblingSum;
 
-                if(curr ->right){
-                    curr -> right -> val = levelSum[i] - siblingSum;
+                if(curr -> left){
+                    curr -> left -> val = siblingSum;
+                    q.push(curr -> left);
+                } 
+
+                if(curr -> right){
+                    curr -> right -> val = siblingSum;
                     q.push(curr -> right);
                 }
             }
-            i++;
+            levelSum = nextSum;
         }
         return root;
     }
