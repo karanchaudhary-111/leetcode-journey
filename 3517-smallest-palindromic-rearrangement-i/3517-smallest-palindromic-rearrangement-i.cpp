b435eeade4e357196e40461b77356b1684696731
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-28 17:01:52 +0530

    Time: 18 ms (91.43%), Space: 54.1 MB (91.65%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -4,9 +4,23 @@ public:
         int n = s.size();
 
         int mid =  n/2;
+        int count[26] = {0};
 
-        sort(s.begin(), s.begin() + mid);
+        // ye  first half ke char ki freq store kiya
+        for(int i = 0; i < mid; i++){
+            count[s[i] - 'a']++;
+        }
 
+        // ye first half ko lexicographically arrange kr diya
+        int idx = 0;
+        for(int i = 0; i < 26; i++){
+            while(count[i] > 0){
+                s[idx++] = (i + 'a');
+                count[i]--;
+            }
+        }
+
+        // ye second half ko set kr diya
         for(int i = 0; i < mid; i++){
             s[n-i-1] = s[i];
         }
