578715c4fffa6942b87d3cca4d44106659f490cc
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-28 23:51:29 +0530

    Time: 35 ms (98.69%), Space: 178.3 MB (91.95%) - LeetHub

--- 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -1,56 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-
-    int countMinToSort(vector<int>& vec){
-
-        int n = vec.size();
-        int swapCount = 0;
-
-        vector<int> sortedVec(vec);
-
-        sort(begin(sortedVec), end(sortedVec));
-
-        unordered_map<int, int> mp;
-
-        for(int i = 0; i < n; i++){
-            mp[vec[i]] = i;
-        }
-
-        for(int i = 0; i < n; i++){
-            if(vec[i] != sortedVec[i]){
-                int currIdx = mp[sortedVec[i]];
-                mp[vec[i]] = currIdx;
-
-                swap(vec[currIdx], vec[i]);
-                swapCount++;
-            }
-        }
-
-        return swapCount;
-    }
-
     int minimumOperations(TreeNode* root) {
-
         queue<TreeNode*> q;
         q.push(root);
-
-        int result = 0;
-
-        while(!q.empty()){
-            int n = q.size();
-            vector<int> levelNode;
-
-            while(n--){
-                TreeNode* curr = q.front();
+        int ans = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+            vector<pair<int, int>> vec;
+            for (int i = 0; i < sz; ++i) {
+                TreeNode* cur = q.front();
                 q.pop();
-
-                levelNode.push_back(curr -> val);
-
-                if(curr -> left) q.push(curr -> left);
-                if(curr -> right) q.push(curr -> right);
+                vec.push_back({cur->val, i});
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
             }
-            result += countMinToSort(levelNode);
+            sort(vec.begin(), vec.end());
+            int idx = 0;
+            while (idx < vec.size()) {
+                if (vec[idx].second == idx) {
+                    idx++;
+                } else {
+                    while (vec[idx].second != idx) {
+                        swap(vec[idx], vec[vec[idx].second]);
+                        ans++;
+                    }
+                }
+            }
         }
-        return result;
+        return ans;
     }
 };
