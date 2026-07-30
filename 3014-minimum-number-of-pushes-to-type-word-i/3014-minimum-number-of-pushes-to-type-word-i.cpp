029ade272881f31a7ac0da1d47dfcf0720b31e78
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-31 00:24:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        
-        int n = word.size();
+       int ans = 0;
 
-        if(n <= 8){
-            return n;
-        }else if(n <= 16){
-            int k = n % 8;
-            if(k == 0){
-                k = 8;
-            }
-            return 8 + (k * 2);
-        }else if(n <= 24){
-            int k = n % 8;
-            if(k == 0) k = 8;
-
-            return 24 + (k * 3);
-        }else {
-            if(n == 25){
-                return 52;
-            }else{
-                return 56;
-            }
+        for(int i = 0;i<word.size();i++){
+            ans+=(i/8)+1;
         }
+        
+        return ans;
     }
 };
