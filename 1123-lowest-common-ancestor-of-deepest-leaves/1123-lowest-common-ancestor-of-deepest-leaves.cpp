cc2c9ea29e2fab0a0dc3f16b7e3b1c76699df373
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-07-31 00:33:09 +0530

    Time: 0 ms (100%), Space: 21.8 MB (98.75%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
+    pair<TreeNode* , int>dfs(TreeNode*root)
+    {
+        if(root == NULL)
+        return {NULL , 0};
 
-    int maxDepth = 0;
-
-    TreeNode* lowestCommonAncestor(TreeNode* root, int maxDepth, int depth) {
-        
-        if(root == NULL) return root;
-
-        if(depth == maxDepth){
-            return root;
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+        if(left.second > right.second)
+        return {left.first , left.second + 1};
+        if(right.second > left.second)
+        {
+            return {right.first , right.second + 1};
         }
-
-        TreeNode* leftN = lowestCommonAncestor(root -> left, maxDepth, depth + 1);
-        TreeNode* rightN = lowestCommonAncestor(root -> right, maxDepth, depth + 1);
-
-        if(leftN != NULL && rightN != NULL){
-            return root;
-        }
-
-        return leftN ? leftN : rightN;
+        return {root , left.second+1};
     }
-
-    void getDepth(TreeNode* root, int depth, unordered_map<TreeNode*, int> mp){
-        if(!root) return ;
-
-        mp[root] = depth;
-        maxDepth = max(maxDepth, depth);
-
-        getDepth(root -> left, depth+1, mp);
-        getDepth(root -> right, depth+1, mp);
-    }
-
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        unordered_map<TreeNode*, int> mp;
-
-        getDepth(root, 0, mp);
-
-        return lowestCommonAncestor(root, maxDepth, 0);
-
-        
+        return dfs(root).first ;
     }
 };
