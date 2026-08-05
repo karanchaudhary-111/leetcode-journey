8e8453a8517340887e2f428186e8a6e8eb49f14b
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-08-06 00:16:45 +0530

    Time: 3 ms (88.45%), Space: 48.4 MB (93.35%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -1,45 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-
-    void inorder(TreeNode* root, vector<int> &vec){
-        if(!root) return ;
-
-        inorder(root -> left, vec);
-        vec.push_back(root -> val);
-
-        inorder(root -> right, vec);
-
+    TreeNode* balanceBST(TreeNode* root) {
+        inorder(root);
+        return sortedArrayToBST(0, sorted_array.size() - 1);
     }
 
-    TreeNode* solve(int l, int r, vector<int> vec){
-        if(l > r) return NULL;
+private:
+    TreeNode* sortedArrayToBST(int start, int end)
+    {
+        if (start > end)
+            return nullptr;
+            
+        int mid = (start + end) / 2;
 
-        int mid = l + (r - l)/2;
-        TreeNode* root = new TreeNode (vec[mid]);
+        TreeNode* root = sorted_array[mid];
 
-        root -> left = solve(l, mid-1, vec);
-        root -> right = solve(mid+1, r, vec);
+        root->left = sortedArrayToBST(start, mid - 1);
+        root->right = sortedArrayToBST(mid + 1, end);
 
         return root;
     }
 
-    TreeNode* balanceBST(TreeNode* root) {
-        
-        vector<int> vec;
-        inorder(root, vec);
-        int n = vec.size();
+    void inorder(TreeNode* node)
+    {
+        if (!node)
+            return;
 
-        return solve(0, n-1, vec);
+        inorder(node->left);
+
+        sorted_array.push_back(node);
+
+        inorder(node->right);
     }
+
+    vector<TreeNode*> sorted_array;
 };
