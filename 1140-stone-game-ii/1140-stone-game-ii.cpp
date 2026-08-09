2f14bc76ea07763836e09dad0b6c3c641ff1fd52
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-08-09 15:45:19 +0530

    Time: 5 ms (83.63%), Space: 13.6 MB (64.48%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int stoneGameII(vector<int>& piles) {
-        vector<vector<int>> memo(piles.size(), vector<int>(piles.size()));
-        vector<int> suffixSum = piles;
-        for (int i = suffixSum.size() - 2; i >= 0; --i)
-            suffixSum[i] += suffixSum[i + 1];
-        return maxStones(suffixSum, 1, 0, memo);
-    }
-
-    int maxStones(vector<int>& suffixSum, int maxTillNow, int currIndex,
-                  vector<vector<int>>& memo) {
-        if (currIndex + 2 * maxTillNow >= suffixSum.size())
-            return suffixSum[currIndex];
-        if (memo[currIndex][maxTillNow] > 0) return memo[currIndex][maxTillNow];
-        int res = INT_MAX;
-
-        for (int i = 1; i <= 2 * maxTillNow; ++i) {
-            res = min(res, maxStones(suffixSum, max(i, maxTillNow),
-                                     currIndex + i, memo));
+    int n;
+    vector<int> suffix;
+    vector<vector<int>> dp;
+    int solve(int i,int M){
+        if(i>=n) return 0;
+        if(i+2*M>=n)
+        return suffix[i];
+        if(dp[i][M]!=-1)
+        return dp[i][M];
+        int ans=0;
+        for(int X=1;X<=2*M;X++){
+            ans=max(ans,suffix[i]-solve(i+X,max(M,X)));
         }
-        memo[currIndex][maxTillNow] = suffixSum[currIndex] - res;
-        return memo[currIndex][maxTillNow];
+        return dp[i][M]=ans;
+    }
+    int stoneGameII(vector<int>& piles) {
+        n=piles.size();
+        suffix.resize(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        suffix[i]=suffix[i+1]+piles[i];
+        dp.assign(n,vector<int>(n+1,-1));
+        return solve(0,1);
     }
 };
